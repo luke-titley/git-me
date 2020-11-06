c1aa90652a801e117a56ac0cdfb04812a7b3c3a4
Luke Titley <luke@titley.com>
2020-11-06 22:47:00 +0100

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use args::*;
 
 //------------------------------------------------------------------------------
 fn main() {
-    //let opt: Options = argh::from_env();
     let tasks: Tasks = argh::from_env();
 
     match tasks.task {
@@ -23,10 +22,14 @@ fn main() {
         }) => {
             println!("start hotfix/{}", name);
         }
-        Task::Release(Release{ branch : Branch::Master(Master{}) }) => {
+        Task::Release(Release {
+            branch: Branch::Master(Master {}),
+        }) => {
             println!("Release patch");
         }
-        Task::Release(Release{ branch : Branch::Develop(Develop{}) }) => {
+        Task::Release(Release {
+            branch: Branch::Develop(Develop {}),
+        }) => {
             println!("Release minor");
         }
         Task::Setup(Setup { server, token }) => {
