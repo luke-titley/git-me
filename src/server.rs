a82c9d49d697bd254f392bd0b11c4ee0e13a5c61
Luke Titley <luke@titley.com>
2020-11-09 22:06:02 +0100

    Implemented the code for discovering the project. Not super efficient, but works

--- src/server.rs
+++ src/server.rs
@@ -4,8 +4,13 @@
 use crate::branch;
 use crate::config;
 
+#[derive(
+    Debug, PartialEq, serde::Serialize, serde::Deserialize, Default, Clone,
+)]
 pub struct Project {
-    pub id: std::string::String,
+    pub id: u32,
+    pub path_with_namespace: std::string::String,
+    pub ssh_url_to_repo: std::string::String,
 }
 
 pub struct Server {
@@ -22,6 +27,22 @@ impl Server {
     }
 
     pub fn project(&self, url: &str) -> Project {
-        Project { id: "".to_string() }
+        let pageable_endpoint = gitlab::api::projects::Projects::builder()
+            .build()
+            .expect("Unable to list all the project in the gitlab server");
+
+        use gitlab::api::Query as _;
+        let projects: Vec<Project> =
+            gitlab::api::paged(pageable_endpoint, gitlab::api::Pagination::All)
+                .query(&self.server)
+                .unwrap();
+
+        for project in projects.iter() {
+            if &project.ssh_url_to_repo == url {
+                return project.clone();
+            }
+        }
+
+        panic!("Unable to find gitlab project for current repo");
     }
 }
