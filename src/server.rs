d7fa4128e98add0705d5a7c063b281fd424b7072
Luke Titley <luke@titley.com>
2020-11-09 19:15:26 +0100

    Create a new server type. That connects to the gitlab server

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,18 @@
 //------------------------------------------------------------------------------
 // from+git_me@luketitley.com
 //------------------------------------------------------------------------------
+use crate::config;
 
-pub fn connect() {}
+pub struct Server {
+    server: gitlab::Gitlab,
+}
 
-pub fn find_repo() {}
+impl Server {
+    pub fn new() -> Self {
+        let config = config::Config::open();
+        Server {
+            server: gitlab::Gitlab::new(&config.server, &config.private_token)
+                .expect("Unable to connect to server"),
+        }
+    }
+}
